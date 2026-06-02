7da18f612d484c110226d704759005476df985d1
Lucas Gillingham <lucasgillingham12@gmail.com>
2026-06-02 11:38:57 -0230

    embed explicit asInvoker manifest in windows setup exe

---
+++ installer/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("cargo:rerun-if-changed=freemid-setup.manifest");
+    println!("cargo:rerun-if-changed=freemid-setup.rc");
+
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "windows" {
+        embed_resource::compile("freemid-setup.rc", embed_resource::NONE);
+    }
+}
